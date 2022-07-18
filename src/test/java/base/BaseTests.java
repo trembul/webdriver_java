2a6adf673ba0d689fafae3eae004d96e127c9d20
trembul <smilech78@gmail.com>
2022-07-18 20:41:56 +0200

    ajajja

---
+++ src/test/java/base/BaseTests.java
@@ -0,0 +1,39 @@
+package base;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class BaseTests {
+
+    private WebDriver driver;
+
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://the-internet.herokuapp.com/");
+
+        WebElement shiftingContentLink = driver.findElement(By.linkText("Shifting Content"));
+        shiftingContentLink.click();
+
+        WebElement example1 = driver.findElement(By.linkText("Example 1: Menu Element"));
+        example1.click();
+
+        List<WebElement> links = driver.findElements(By.tagName("li"));
+        System.out.println(links.size());
+
+
+
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+    public static void main(String[] args) {
+        BaseTests test = new BaseTests();
+        test.setUp();
+    }
+}
