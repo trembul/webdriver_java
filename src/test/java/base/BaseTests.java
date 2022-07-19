ca2957eba217cf84929734a50910851b8132938e
trembul <smilech78@gmail.com>
2022-07-19 20:46:57 +0200

    lekcja 4

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -5,30 +5,22 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        WebElement shiftingContentLink = driver.findElement(By.linkText("Shifting Content"));
-        shiftingContentLink.click();
+        homePage = new HomePage(driver);
 
-        WebElement example1 = driver.findElement(By.linkText("Example 1: Menu Element"));
-        example1.click();
-
-        List<WebElement> links = driver.findElements(By.tagName("li"));
-        System.out.println(links.size());
-
-
-
-        System.out.println(driver.getTitle());
         driver.quit();
     }
 
